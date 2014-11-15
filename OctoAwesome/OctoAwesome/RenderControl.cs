c27ac7cc5d19184b4368954a1923602474af6724
tomwendel_cp <tom.wendel@rabbitcode.de>
2014-11-15 14:05:48 +0000

    [#008] Unsere eigene Vector-Klasse

--- OctoAwesome/OctoAwesome/RenderControl.cs
+++ OctoAwesome/OctoAwesome/RenderControl.cs
@@ -8,17 +8,18 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Diagnostics;
+using OctoAwesome.Model;
 
 namespace OctoAwesome
 {
     public partial class RenderControl : UserControl
     {
         private int SPRITE_WIDTH = 57;
-        private int SPRITE_HEIGHT = 64;
+        private int SPRITE_HEIGHT = 57;
 
         private Stopwatch watch = new Stopwatch();
 
-        public Game Game { get; set; }
+        internal Game Game { get; set; }
 
         private Image grass;
         private Image sprite;
@@ -59,11 +60,20 @@ namespace OctoAwesome
 
             using (Brush brush = new SolidBrush(Color.White))
             {
-                int frame = (int)((watch.ElapsedMilliseconds / 250) % 8);
+                int frame = (int)((watch.ElapsedMilliseconds / 250) % 4);
+
+                int offsetx = 0;
+                switch (frame)
+                {
+                    case 0: offsetx = 0; break;
+                    case 1: offsetx = SPRITE_WIDTH; break;
+                    case 2: offsetx = 2 * SPRITE_WIDTH; break;
+                    case 3: offsetx = SPRITE_WIDTH; break;
+                }
 
                 e.Graphics.DrawImage(sprite,
-                    new Rectangle(Game.Position.X, Game.Position.Y, SPRITE_WIDTH, SPRITE_HEIGHT), 
-                    new Rectangle(frame * SPRITE_WIDTH, 0, SPRITE_WIDTH, SPRITE_HEIGHT), 
+                    new RectangleF(Game.Player.Position.X, Game.Player.Position.Y, SPRITE_WIDTH, SPRITE_HEIGHT), 
+                    new RectangleF(offsetx, 0, SPRITE_WIDTH, SPRITE_HEIGHT), 
                     GraphicsUnit.Pixel);
 
                 // e.Graphics.FillEllipse(brush, new Rectangle(Game.Position.X, Game.Position.Y, 100, 100));
