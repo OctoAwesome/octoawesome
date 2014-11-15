91ccbca8a21b620a0a433faf2f1dcec9288487de
tomwendel_cp <SND\tomwendel_cp>
2014-11-15 14:32:20 +0000

    [#009] Sprite Animation mit Abhängigkeit der Richtung und Player States

--- OctoAwesome/OctoAwesome/RenderControl.cs
+++ OctoAwesome/OctoAwesome/RenderControl.cs
@@ -63,17 +63,44 @@ namespace OctoAwesome
                 int frame = (int)((watch.ElapsedMilliseconds / 250) % 4);
 
                 int offsetx = 0;
-                switch (frame)
+                if (Game.Player.State == PlayerState.Walk)
                 {
-                    case 0: offsetx = 0; break;
-                    case 1: offsetx = SPRITE_WIDTH; break;
-                    case 2: offsetx = 2 * SPRITE_WIDTH; break;
-                    case 3: offsetx = SPRITE_WIDTH; break;
+                    switch (frame)
+                    {
+                        case 0: offsetx = 0; break;
+                        case 1: offsetx = SPRITE_WIDTH; break;
+                        case 2: offsetx = 2 * SPRITE_WIDTH; break;
+                        case 3: offsetx = SPRITE_WIDTH; break;
+                    }
+                }
+                else
+                {
+                    offsetx = SPRITE_WIDTH;
+                }
+
+                // Umrechung in Grad
+                float direction = (Game.Player.Angle * 360f) / (float)(2 * Math.PI);
+
+                // In positiven Bereich
+                direction += 180;
+
+                // Offset
+                direction += 45;
+
+                int sector = (int)(direction / 90);
+
+                int offsety = 0;
+                switch (sector)
+                {
+                    case 1: offsety = 3 * SPRITE_HEIGHT; break;
+                    case 2: offsety = 2 * SPRITE_HEIGHT; break;
+                    case 3: offsety = 0 * SPRITE_HEIGHT; break;
+                    case 4: offsety = 1 * SPRITE_HEIGHT; break;
                 }
 
                 e.Graphics.DrawImage(sprite,
                     new RectangleF(Game.Player.Position.X, Game.Player.Position.Y, SPRITE_WIDTH, SPRITE_HEIGHT), 
-                    new RectangleF(offsetx, 0, SPRITE_WIDTH, SPRITE_HEIGHT), 
+                    new RectangleF(offsetx, offsety, SPRITE_WIDTH, SPRITE_HEIGHT), 
                     GraphicsUnit.Pixel);
 
                 // e.Graphics.FillEllipse(brush, new Rectangle(Game.Position.X, Game.Position.Y, 100, 100));
