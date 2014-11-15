def3e01d86b75b32a5b12c611ac0d5f03521bf8b
tomwendel_cp <tom.wendel@rabbitcode.de>
2014-11-15 11:44:19 +0000

    [#005] Textur eingefügt, Pfeiltastennavigation

--- OctoAwesome/OctoAwesome/MainForm.cs
+++ OctoAwesome/OctoAwesome/MainForm.cs
@@ -19,9 +19,34 @@ namespace OctoAwesome
         public MainForm()
         {
             InitializeComponent();
+
             watch.Start();
             renderControl.Game = game;
             game.PlaygroundSize = new Point(renderControl.ClientSize.Width, renderControl.ClientSize.Height);
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (game != null)
+            {
+                switch (keyData)
+                {
+                    case Keys.Left:
+                        game.Left = true;
+                        break;
+                    case Keys.Right:
+                        game.Right = true;
+                        break;
+                    case Keys.Up:
+                        game.Up = true;
+                        break;
+                    case Keys.Down:
+                        game.Down = true;
+                        break;
+                }
+            }
+
+            return base.ProcessCmdKey(ref msg, keyData);
         }
 
         private void timer_Tick(object sender, EventArgs e)
@@ -34,6 +59,30 @@ namespace OctoAwesome
         private void closeMenu_Click(object sender, EventArgs e)
         {
             Close();
+        }
+
+        protected override void OnKeyUp(KeyEventArgs e)
+        {
+            if (game == null)
+                return;
+
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    game.Left = false;
+                    break;
+                case Keys.Right:
+                    game.Right = false;
+                    break;
+                case Keys.Up:
+                    game.Up = false;
+                    break;
+                case Keys.Down:
+                    game.Down = false;
+                    break;
+            }
+
+            base.OnKeyUp(e);
         }
     }
 }
