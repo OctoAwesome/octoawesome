234b0ced08631d6a8ef467e30dc25a4aa6ac9bc2
tomwendel_cp <SND\tomwendel_cp>
2014-11-20 18:04:57 +0000

    [#013] Automatische Kamerafahrt

--- OctoAwesome/OctoAwesome/Model/Player.cs
+++ OctoAwesome/OctoAwesome/Model/Player.cs
@@ -12,7 +12,7 @@ namespace OctoAwesome.Model
     {
         private Input input;
 
-        public readonly float MAXSPEED = 100f;
+        public readonly float MAXSPEED = 300f;
 
         public Vector2 Position { get; set; }
 
