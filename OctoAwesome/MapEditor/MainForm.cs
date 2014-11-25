836f3400b9d9052f8b4ead3f4cf69ef83a412fb5
tomwendel_cp <SND\tomwendel_cp>
2014-11-25 17:43:28 +0000

    [#017] Fine Tuning des Editors

--- OctoAwesome/MapEditor/MainForm.cs
+++ OctoAwesome/MapEditor/MainForm.cs
@@ -14,6 +14,12 @@ namespace MapEditor
     public partial class MainForm : Form
     {
         private Map map;
+        private int cellSize = 20;
+        private CellType drawMode = CellType.Gras;
+
+        private bool mouseActive = false;
+        private Point mousePosition = new Point();
+        private bool mouseDraw = false;
 
         public MainForm()
         {
@@ -22,10 +28,27 @@ namespace MapEditor
             timer.Enabled = true;
         }
 
-        private void renderPanel_Paint(object sender, PaintEventArgs e)
-        {
-            int cellSize = 20;
+        
 
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            renderControl.Invalidate();
+            cellPositionLabel.Text = (mouseActive ? mousePosition.X + "/" + mousePosition.Y : string.Empty);
+            saveMenu.Enabled = map != null;
+        }
+
+        private void smallMapMenu_Click(object sender, EventArgs e)
+        {
+            map = new Map(20, 20);
+        }
+
+        private void mediumMapMenu_Click(object sender, EventArgs e)
+        {
+            map = new Map(40, 40);
+        }
+
+        private void renderControl_Paint(object sender, PaintEventArgs e)
+        {
             e.Graphics.Clear(Color.CornflowerBlue);
 
             if (map == null)
@@ -34,6 +57,7 @@ namespace MapEditor
             SolidBrush sandBrush = new SolidBrush(Color.SandyBrown);
             SolidBrush grasBrush = new SolidBrush(Color.DarkGreen);
             SolidBrush waterBrush = new SolidBrush(Color.Blue);
+            SolidBrush selectionBrush = new SolidBrush(Color.FromArgb(100, Color.White));
 
             for (int x = 0; x < map.Cells.GetLength(0); x++)
             {
@@ -60,17 +84,22 @@ namespace MapEditor
                 }
             }
 
-            using (Pen pen = new Pen(Color.White))
+            using (Pen pen = new Pen(Color.FromArgb(100, Color.White)))
             {
-                for (int x = 0; x < map.Cells.GetLength(0); x++)
+                for (int x = 1; x < map.Cells.GetLength(0) + 1; x++)
                 {
                     e.Graphics.DrawLine(pen, new Point(x * cellSize, 0), new Point(x * cellSize, map.Cells.GetLength(1) * cellSize));
                 }
 
-                for (int y = 0; y < map.Cells.GetLength(1); y++)
+                for (int y = 1; y < map.Cells.GetLength(1) + 1; y++)
                 {
                     e.Graphics.DrawLine(pen, new Point(0, y * cellSize), new Point(map.Cells.GetLength(0) * cellSize, y * cellSize));
                 }
+            }
+
+            if (mouseActive)
+            {
+                e.Graphics.FillRectangle(selectionBrush, new Rectangle(mousePosition.X * cellSize, mousePosition.Y * cellSize, cellSize, cellSize));
             }
 
             sandBrush.Dispose();
@@ -78,19 +107,87 @@ namespace MapEditor
             waterBrush.Dispose();
         }
 
-        private void timer1_Tick(object sender, EventArgs e)
+        private void renderControl_MouseEnter(object sender, EventArgs e)
         {
-            renderPanel.Invalidate();
+            mouseActive = true;
         }
 
-        private void smallMapMenu_Click(object sender, EventArgs e)
+        private void renderControl_MouseLeave(object sender, EventArgs e)
         {
-            map = new Map(20, 20);
+            mouseActive = false;
         }
 
-        private void mediumMapMenu_Click(object sender, EventArgs e)
+        private void renderControl_MouseMove(object sender, MouseEventArgs e)
         {
-            map = new Map(40, 40);
+            mousePosition = new Point((int)(e.X / cellSize), (int)(e.Y / cellSize));
+
+            if (map == null || !mouseDraw || !mouseActive)
+                return;
+
+            if (mousePosition.X < 0 || mousePosition.X >= map.Cells.GetLength(0) ||
+                mousePosition.Y < 0 || mousePosition.Y >= map.Cells.GetLength(1))
+                return;
+
+            map.Cells[mousePosition.X, mousePosition.Y] = drawMode;
+        }
+
+        private void sandButton_Click(object sender, EventArgs e)
+        {
+            drawMode = CellType.Sand;
+            grasButton.Checked = false;
+            sandButton.Checked = true;
+            waterButton.Checked = false;
+        }
+
+        private void grasButton_Click(object sender, EventArgs e)
+        {
+            drawMode = CellType.Gras;
+            grasButton.Checked = true;
+            sandButton.Checked = false;
+            waterButton.Checked = false;
+        }
+
+        private void waterButton_Click(object sender, EventArgs e)
+        {
+            drawMode = CellType.Water;
+            grasButton.Checked = false;
+            sandButton.Checked = false;
+            waterButton.Checked = true;
+        }
+
+        private void renderControl_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                mouseDraw = true;
+            }
+        }
+
+        private void renderControl_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                mouseDraw = false;
+            }
+        }
+
+        private void saveMenu_Click(object sender, EventArgs e)
+        {
+            if (map == null)
+                return;
+
+            if (saveFileDialog.ShowDialog(this) == System.Windows.Forms.DialogResult.OK)
+            {
+                Map.Save(saveFileDialog.FileName, map);
+            }
+        }
+
+        private void loadMenu_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog(this) == System.Windows.Forms.DialogResult.OK)
+            {
+                map = Map.Load(openFileDialog.FileName);
+            }
         }
     }
 }
