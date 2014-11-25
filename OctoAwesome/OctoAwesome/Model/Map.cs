79542abdaf21788d5455f75fccd46d0a2e2260ba
tomwendel_cp <tom.wendel@rabbitcode.de>
2014-11-25 17:16:27 +0000

    [#016] Map Editor hinzugefügt

--- OctoAwesome/OctoAwesome/Model/Map.cs
+++ OctoAwesome/OctoAwesome/Model/Map.cs
@@ -1,36 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace OctoAwesome.Model
 {
-    internal sealed class Map
+    [Serializable]
+    public sealed class Map
     {
         public const int CELLSIZE = 100;
 
         public CellType[,] Cells { get; private set; }
 
-        public Map()
+        public Map(int width, int height)
         {
-            Cells = new CellType[20, 20];
+            Cells = new CellType[width, height];
 
-            for (int x = 0; x < 20; x++)
+            for (int x = 0; x < width; x++)
             {
-                for (int y = 0; y < 20; y++)
+                for (int y = 0; y < height; y++)
                 {
-                    if (x > 5 && x < 15 || y > 5 && y < 15)
-                    {
-                        Cells[x, y] = CellType.Sand;
-                    }
-                    else
-                    {
+                    //if (x > 5 && x < 15 || y > 5 && y < 15)
+                    //{
+                    //    Cells[x, y] = CellType.Sand;
+                    //}
+                    //else
+                    //{
                         Cells[x, y] = CellType.Gras;
-                    }
+                    //}
                 }
             }
         }
+
+        #region Loader
+
+        public static Map Load(string filename)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Map));
+            using (Stream stream = File.OpenRead(filename))
+            {
+                return (Map)serializer.Deserialize(stream);
+            }
+        }
+
+        public static void Save(string filename, Map map)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Map));
+            using (Stream stream = File.OpenWrite(filename))
+            {
+                serializer.Serialize(stream, map);
+            }
+        }
+
+        #endregion
     }
 
     public enum CellType
