378a8147b9c0bd84d65f53677557aafc18cdca2a
tomwendel_cp <tom.wendel@rabbitcode.de>
2014-11-30 17:44:43 +0000

    [#025] Feinschliff CellTypes

---
+++ OctoAwesome/OctoAwesome/Model/CellTypeDefintion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OctoAwesome.Model
+{
+    public sealed class CellTypeDefintion
+    {
+        public float VelocityFactor { get; set; }
+
+        public bool CanGoto { get; set; }
+    }
+}
