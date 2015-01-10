2d2996d0eed14057cbbc7e9c1fa2b748b04d455d
tomwendel_cp <SND\tomwendel_cp>
2015-01-10 02:10:23 +0000

    [#037] MonoGame Port

---
+++ OctoAwesome/OctoAwesomeDX/Components/Camera.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Xna.Framework;
+using OctoAwesome.Model;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OctoAwesome.Components
+{
+    internal sealed class Camera
+    {
+        private OctoAwesome.Model.Game game;
+        private Input2 input;
+        private Vector2 renderSize;
+
+        public readonly float MAXSPEED = 1000f;
+
+        public readonly float SCALE = 64f;
+
+        public Camera(OctoAwesome.Model.Game game, Input2 input)
+        {
+            this.game = game;
+            this.input = input;
+        }
+
+        public void SetRenderSize(Vector2 renderSize)
+        {
+            this.renderSize = renderSize;
+            RecalcViewPort();
+        }
+
+        public void Update(GameTime frameTime)
+        {
+            //Vector2 velocity = new Vector2(
+            //    (input.CamLeft ? -1f : 0f) + (input.CamRight ? 1f : 0f),
+            //    (input.CamUp ? -1f : 0f) + (input.CamDown ? 1f : 0f));
+
+            //velocity = velocity.Normalized();
+
+            //Center += (velocity * MAXSPEED * (float)frameTime.TotalSeconds);
+
+            float posX = (game.Player.Position.X * SCALE) - ViewPort.Left;
+            float posY = (game.Player.Position.Y * SCALE) - ViewPort.Top;
+
+            float frameX = ViewPort.Width / 4;
+            float frameY = ViewPort.Height / 4;
+
+            if (posX < frameX)
+                Center = new Vector2(Center.X - (frameX - posX), Center.Y);
+
+            if (posX > ViewPort.Width - frameX)
+                Center = new Vector2(Center.X + (posX - (ViewPort.Width - frameX)), Center.Y);
+
+            if (posY < frameY)
+                Center = new Vector2(Center.X, Center.Y - (frameY - posY));
+
+            if (posY > ViewPort.Height - frameY)
+                Center = new Vector2(Center.X, Center.Y + (posY - (ViewPort.Height - frameY)));
+
+
+            if (Center.X < (ViewPort.Width / 2) - 100)
+                Center = new Vector2((ViewPort.Width / 2) - 100, Center.Y);
+
+            if (Center.Y < (ViewPort.Height / 2) - 100)
+                Center = new Vector2(Center.X, (ViewPort.Height / 2) - 100);
+
+            if (Center.X > (game.PlaygroundSize.X * SCALE) - (ViewPort.Width / 2) + 100)
+                Center = new Vector2((game.PlaygroundSize.X * SCALE) - (ViewPort.Width / 2) + 100, Center.Y);
+
+            if (Center.Y > (game.PlaygroundSize.Y * SCALE) - (ViewPort.Height / 2) + 100)
+                Center = new Vector2(Center.X, (game.PlaygroundSize.Y * SCALE) - (ViewPort.Height / 2) + 100);
+
+            RecalcViewPort();
+        }
+
+        private void RecalcViewPort()
+        {
+            float offsetX = (game.Camera.Center.X) - (this.renderSize.X / 2);
+            float offsetY = (game.Camera.Center.Y) - (this.renderSize.Y / 2);
+
+            ViewPort = new Rectangle((int)offsetX, (int)offsetY, (int)renderSize.X, (int)renderSize.Y);
+        }
+
+        /// <summary>
+        /// Kameraposition (Render-Koordinate)
+        /// </summary>
+        public Vector2 Center { get; private set; }
+
+        /// <summary>
+        /// Sichtbarer Bereich (Render-Koordinate)
+        /// </summary>
+        public Rectangle ViewPort { get; set; }
+    }
+}
---
+++ OctoAwesome/OctoAwesomeDX/Rendering/CellTypeRenderer.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using OctoAwesome.Components;
+using OctoAwesome.Model;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OctoAwesome.Rendering
+{
+    internal class CellTypeRenderer
+    {
+        private readonly Texture2D center;
+        private readonly Texture2D left;
+        private readonly Texture2D right;
+        private readonly Texture2D upper;
+        private readonly Texture2D lower;
+        private readonly Texture2D upperLeft_concave;
+        private readonly Texture2D upperRight_concave;
+        private readonly Texture2D lowerLeft_concave;
+        private readonly Texture2D lowerRight_concave;
+        private readonly Texture2D upperLeft_convex;
+        private readonly Texture2D upperRight_convex;
+        private readonly Texture2D lowerLeft_convex;
+        private readonly Texture2D lowerRight_convex;
+
+        public CellTypeRenderer(ContentManager content, string name)
+        {
+            center = content.Load<Texture2D>(string.Format("Textures/{0}_center", name));
+            left = content.Load<Texture2D>(string.Format("Textures/{0}_left", name)); 
+            right = content.Load<Texture2D>(string.Format("Textures/{0}_right", name)); 
+            upper = content.Load<Texture2D>(string.Format("Textures/{0}_upper", name)); 
+            lower = content.Load<Texture2D>(string.Format("Textures/{0}_lower", name)); 
+            upperLeft_concave = content.Load<Texture2D>(string.Format("Textures/{0}_upperLeft_concave", name));
+            upperRight_concave = content.Load<Texture2D>(string.Format("Textures/{0}_upperRight_concave", name));
+            lowerLeft_concave = content.Load<Texture2D>(string.Format("Textures/{0}_lowerLeft_concave", name));
+            lowerRight_concave = content.Load<Texture2D>(string.Format("Textures/{0}_lowerRight_concave", name));
+            upperLeft_convex = content.Load<Texture2D>(string.Format("Textures/{0}_upperLeft_convex", name));
+            upperRight_convex = content.Load<Texture2D>(string.Format("Textures/{0}_upperRight_convex", name));
+            lowerLeft_convex = content.Load<Texture2D>(string.Format("Textures/{0}_lowerLeft_convex", name));
+            lowerRight_convex = content.Load<Texture2D>(string.Format("Textures/{0}_lowerRight_convex", name)); 
+        }
+
+        public void Draw(SpriteBatch g, OctoAwesome.Model.Game game, int x, int y)
+        {
+            CellType centerType = game.Map.GetCell(x, y);
+
+            g.Draw(center, new Rectangle(
+                (int)(x * game.Camera.SCALE - game.Camera.ViewPort.X),
+                (int)(y * game.Camera.SCALE - game.Camera.ViewPort.Y),
+                (int)game.Camera.SCALE,
+                (int)game.Camera.SCALE), Color.White);
+
+            bool emptyLeft =
+                x > 0 &&
+                game.Map.GetCell(x - 1, y) != centerType;
+            bool emptyTop =
+                y > 0 &&
+                game.Map.GetCell(x, y - 1) != centerType;
+            bool emptyRight =
+                (x + 1) < game.Map.Columns &&
+                game.Map.GetCell(x + 1, y) != centerType;
+            bool emptyBottom =
+                (y + 1) < game.Map.Rows &&
+                game.Map.GetCell(x, y + 1) != centerType;
+
+            bool emptyUpperLeft =
+                x > 0 &&
+                y > 0 &&
+                game.Map.GetCell(x - 1, y - 1) != centerType;
+            bool emptyUpperRight =
+                (x + 1) < game.Map.Columns &&
+                y > 0 &&
+                game.Map.GetCell(x + 1, y - 1) != centerType;
+            bool emptyLowerLeft =
+                x > 0 &&
+                (y + 1) < game.Map.Rows &&
+                game.Map.GetCell(x - 1, y + 1) != centerType;
+            bool emptyLowerRight =
+                (x + 1) < game.Map.Columns &&
+                (y + 1) < game.Map.Rows &&
+                game.Map.GetCell(x + 1, y + 1) != centerType;
+
+            // Gerade Kanten
+            if (emptyLeft) DrawTexture(g, game.Camera, x, y, left);
+            if (emptyRight) DrawTexture(g, game.Camera, x, y, right);
+            if (emptyTop) DrawTexture(g, game.Camera, x, y, upper);
+            if (emptyBottom) DrawTexture(g, game.Camera, x, y, lower);
+
+            // Konvexe Ecken
+            if (emptyLeft && emptyTop) DrawTexture(g, game.Camera, x, y, upperLeft_convex);
+            if (emptyLeft && emptyBottom) DrawTexture(g, game.Camera, x, y, lowerLeft_convex);
+            if (emptyRight && emptyTop) DrawTexture(g, game.Camera, x, y, upperRight_convex);
+            if (emptyRight && emptyBottom) DrawTexture(g, game.Camera, x, y, lowerRight_convex);
+
+            if (emptyUpperLeft && !emptyLeft && !emptyTop) DrawTexture(g, game.Camera, x, y, upperLeft_concave);
+            if (emptyUpperRight && !emptyRight && !emptyTop) DrawTexture(g, game.Camera, x, y, upperRight_concave);
+            if (emptyLowerLeft && !emptyLeft && !emptyBottom) DrawTexture(g, game.Camera, x, y, lowerLeft_concave);
+            if (emptyLowerRight && !emptyRight && !emptyBottom) DrawTexture(g, game.Camera, x, y, lowerRight_concave);
+        }
+
+        private static void DrawTexture(SpriteBatch g, Camera camera, int x, int y, Texture2D image)
+        {
+            g.Draw(image, new Rectangle(
+                (int)(x * camera.SCALE - camera.ViewPort.X),
+                (int)(y * camera.SCALE - camera.ViewPort.Y),
+                (int)camera.SCALE,
+                (int)camera.SCALE), Color.White);
+        }
+    }
+}
