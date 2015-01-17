6774c8f1c3c08de37ec5b7631764dbfb8022b2c3
tomwendel_cp <SND\tomwendel_cp>
2015-01-17 15:39:16 +0000

    [#044] Karte rendern in 3D

--- OctoAwesome/OctoAwesomeDX/Components/Render3DComponent.cs
+++ OctoAwesome/OctoAwesomeDX/Components/Render3DComponent.cs
@@ -9,47 +9,69 @@ namespace OctoAwesome.Components
 {
     internal sealed class Render3DComponent : DrawableGameComponent
     {
-        BasicEffect effect;
-        Texture2D sand;
+        private WorldComponent world;
 
-        VertexBuffer vb;
-        IndexBuffer ib;
+        private BasicEffect effect;
+        private Texture2D grass;
 
-        public Render3DComponent(Game game)
+        private VertexBuffer vb;
+        private IndexBuffer ib;
+        private int vertexCount;
+        private int indexCount;
+
+        public Render3DComponent(Game game, WorldComponent world)
             : base(game)
         {
-
+            this.world = world;
         }
 
         protected override void LoadContent()
         {
-            VertexPositionNormalTexture[] vertices = new VertexPositionNormalTexture[] {
-                new VertexPositionNormalTexture(new Vector3(-1, 1, 0), Vector3.Forward, new Vector2(0, 0)),
-                new VertexPositionNormalTexture(new Vector3(1, 1, 0), Vector3.Forward, new Vector2(1, 0)),
-                new VertexPositionNormalTexture(new Vector3(1, -1, 0), Vector3.Forward, new Vector2(1, 1)),
-                new VertexPositionNormalTexture(new Vector3(-1, -1, 0), Vector3.Forward, new Vector2(0, 1)),
-            };
+            int width = world.World.Map.CellCache.GetLength(0);
+            int height = world.World.Map.CellCache.GetLength(1);
+            vertexCount = width * height * 4;
+            indexCount = width * height * 6;
 
-            short[] index = new short[] {
-                0, 1, 2, 0, 2, 3
-            };
+            VertexPositionNormalTexture[] vertices = new VertexPositionNormalTexture[vertexCount];
+            short[] index = new short[indexCount];
 
-            vb = new VertexBuffer(GraphicsDevice, VertexPositionNormalTexture.VertexDeclaration, 6, BufferUsage.WriteOnly);
+            for (int z = 0; z < height; z++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    int vertexOffset = (((z * width) + x) * 4);
+                    int indexOffset = (((z * width) + x) * 6);
+
+                    vertices[vertexOffset + 0] = new VertexPositionNormalTexture(new Vector3(x, 0, z), Vector3.Up, new Vector2(0, 0));
+                    vertices[vertexOffset + 1] = new VertexPositionNormalTexture(new Vector3(x + 1, 0, z), Vector3.Up, new Vector2(1, 0));
+                    vertices[vertexOffset + 2] = new VertexPositionNormalTexture(new Vector3(x, 0, z + 1), Vector3.Up, new Vector2(0, 1));
+                    vertices[vertexOffset + 3] = new VertexPositionNormalTexture(new Vector3(x + 1, 0, z + 1), Vector3.Up, new Vector2(1, 1));
+
+                    index[indexOffset + 0] = (short)(vertexOffset + 0);
+                    index[indexOffset + 1] = (short)(vertexOffset + 1);
+                    index[indexOffset + 2] = (short)(vertexOffset + 3);
+                    index[indexOffset + 3] = (short)(vertexOffset + 0);
+                    index[indexOffset + 4] = (short)(vertexOffset + 3);
+                    index[indexOffset + 5] = (short)(vertexOffset + 2);
+                }
+            }
+
+            vb = new VertexBuffer(GraphicsDevice, VertexPositionNormalTexture.VertexDeclaration, vertexCount, BufferUsage.WriteOnly);
             vb.SetData<VertexPositionNormalTexture>(vertices);
 
-            ib = new IndexBuffer(GraphicsDevice, IndexElementSize.SixteenBits, 6, BufferUsage.WriteOnly);
+            ib = new IndexBuffer(GraphicsDevice, IndexElementSize.SixteenBits, indexCount, BufferUsage.WriteOnly);
             ib.SetData<short>(index);
 
-            sand = Game.Content.Load<Texture2D>("Textures/sand_center");
+            grass = Game.Content.Load<Texture2D>("Textures/grass_center");
 
             effect = new BasicEffect(GraphicsDevice);
             effect.World = Matrix.Identity;
-            effect.View = Matrix.CreateLookAt(new Vector3(0, 0, 5), Vector3.Zero, Vector3.Up);
+            effect.View = Matrix.CreateLookAt(new Vector3(0, 20, 0), Vector3.Zero, Vector3.Forward);
             effect.Projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4, GraphicsDevice.Viewport.AspectRatio, 1f, 10000f);
             effect.TextureEnabled = true;
-            effect.Texture = sand;
+            effect.Texture = grass;
 
-            effect.EnableDefaultLighting();
+            // effect.EnableDefaultLighting();
             //effect.LightingEnabled = true;
             //effect.AmbientLightColor = Color.DarkGray.ToVector3();
 
@@ -61,12 +83,8 @@ namespace OctoAwesome.Components
             base.LoadContent();
         }
 
-        float rotY = 0f;
-
         public override void Update(GameTime gameTime)
         {
-            rotY += (float)gameTime.ElapsedGameTime.TotalSeconds;
-
             base.Update(gameTime);
         }
 
@@ -77,14 +95,14 @@ namespace OctoAwesome.Components
             GraphicsDevice.RasterizerState.CullMode = CullMode.None;
             // GraphicsDevice.RasterizerState.FillMode = FillMode.WireFrame;
 
-            effect.World = Matrix.CreateRotationY(rotY);
+            effect.World = Matrix.Identity;
             GraphicsDevice.SetVertexBuffer(vb);
             GraphicsDevice.Indices = ib;
 
             foreach (var pass in effect.CurrentTechnique.Passes)
             {
                 pass.Apply();
-                GraphicsDevice.DrawIndexedPrimitives(PrimitiveType.TriangleList, 0, 0, 4, 0, 2);
+                GraphicsDevice.DrawIndexedPrimitives(PrimitiveType.TriangleList, 0, 0, vertexCount, 0, indexCount / 3);
             }
         }
     }
