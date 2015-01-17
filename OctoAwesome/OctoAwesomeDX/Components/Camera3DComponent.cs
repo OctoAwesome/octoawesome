a33b2a62147b763cbc3737f7ddf615160d8862a6
tomwendel_cp <SND\tomwendel_cp>
2015-01-17 17:42:47 +0000

    [#047] Items united. Endlich wieder Wasser

--- OctoAwesome/OctoAwesomeDX/Components/Camera3DComponent.cs
+++ OctoAwesome/OctoAwesomeDX/Components/Camera3DComponent.cs
@@ -27,11 +27,18 @@ namespace OctoAwesome.Components
 
         public override void Update(GameTime gameTime)
         {
+            CameraPosition = new Vector3(world.World.Player.Position.X, 10, world.World.Player.Position.Y + 10);
+            CameraUpVector = Vector3.Up;
+
             View = Matrix.CreateLookAt(
-                new Vector3(world.World.Player.Position.X, 10, world.World.Player.Position.Y + 35), 
-                new Vector3(world.World.Player.Position.X, 0, world.World.Player.Position.Y), 
-                Vector3.Up);
+                CameraPosition, 
+                new Vector3(world.World.Player.Position.X, 0, world.World.Player.Position.Y),
+                CameraUpVector);
         }
+
+        public Vector3 CameraPosition { get; private set; }
+
+        public Vector3 CameraUpVector { get; private set; }
 
         public Matrix View { get; private set; }
 
