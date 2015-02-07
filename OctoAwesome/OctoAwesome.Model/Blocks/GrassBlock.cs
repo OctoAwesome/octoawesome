eafa37fda471e8b15e1856214f41f5e3d343f23b
tomwendel_cp <SND\tomwendel_cp>
2015-02-07 16:42:57 +0000

    [#062] Sinus-Kartengerator

--- OctoAwesome/OctoAwesome.Model/Blocks/GrassBlock.cs
+++ OctoAwesome/OctoAwesome.Model/Blocks/GrassBlock.cs
@@ -1,4 +1,5 @@
-﻿using OctoAwesome.Model.Properties;
+﻿using Microsoft.Xna.Framework;
+using OctoAwesome.Model.Properties;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
@@ -7,7 +8,7 @@ using System.Text;
 
 namespace OctoAwesome.Model.Blocks
 {
-    public class GrassBlock : IBlock
+    public class GrassBlock : BaseBlock
     {
         public static Bitmap Texture { get { return Resources.grass_center; } }
     }
