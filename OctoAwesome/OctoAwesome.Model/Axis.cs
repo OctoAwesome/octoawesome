7b331ad36b1c4f9cd0899375f4eff0ff6b249b95
tomwendel_cp <tom.wendel@rabbitcode.de>
2015-02-28 00:58:48 +0000

    [079] verhunzte Kollision

---
+++ OctoAwesome/OctoAwesome.Model/Axis.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OctoAwesome.Model
+{
+    [Flags]
+    public enum Axis
+    {
+        None = 0,
+        X = 1,
+        Y = 2,
+        Z = 4
+    }
+}
