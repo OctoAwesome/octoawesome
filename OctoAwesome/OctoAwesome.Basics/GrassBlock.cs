b0ecb356e9be81a440ae01eb40d755bda0ed5d2d
tomwendel_cp <tom.wendel@rabbitcode.de>
2015-05-28 22:30:37 +0000

    Restliche Blöcke angepasst

--- OctoAwesome/OctoAwesome.Basics/GrassBlock.cs
+++ OctoAwesome/OctoAwesome.Basics/GrassBlock.cs
@@ -8,5 +8,15 @@ namespace OctoAwesome.Basics
 {
     public class GrassBlock : Block
     {
+        public GrassBlock()
+        {
+            TopTexture = 0;
+            BottomTexture = 1;
+
+            NorthTexture = 2;
+            SouthTexture = 2;
+            WestTexture = 2;
+            EastTexture = 2;
+        }
     }
 }
