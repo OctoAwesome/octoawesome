1cb5705321bd0638f6f4b3aff1493509594f8ffb
Paul Reichelt <paul.reichelt@outlook.com>
2015-08-20 21:12:53 +0200

    Implemented Ring Cache for ChunkLoading

--- OctoAwesome/OctoAwesome/ChunkCache.cs
+++ OctoAwesome/OctoAwesome/ChunkCache.cs
@@ -5,44 +5,68 @@ namespace OctoAwesome
 {
     public class ChunkCache : IChunkCache
     {
-        private readonly IDictionary<Index3, IChunk> _chunks = new Dictionary<Index3, IChunk>();
+        //private readonly IDictionary<Index3, IChunk> _chunks = new Dictionary<Index3, IChunk>();
+        private readonly IChunk[] _chunks;
         private readonly Func<Index3, IChunk> _loadDelegate;
         private readonly Action<Index3, IChunk> _saveDelegate;
+
+        private const int XLimit = 5;
+        private const int YLimit = 5;
+        //private const int ZLimit = 5;
+
+        private const int XMask = 31;
+        private const int YMask = 31;
+        private const int ZMask = 31;
+
 
         public ChunkCache(Func<Index3, IChunk> loadDelegate, Action<Index3, IChunk> saveDelegate)
         {
             _saveDelegate = saveDelegate;
             _loadDelegate = loadDelegate;
+
+            _chunks = new IChunk[(XMask+1) * (YMask+1) * (ZMask+1)];
         }
 
         public IChunk Get(Index3 idx)
         {
-            if (!_chunks.ContainsKey(idx))
-                return null;
-
-            return _chunks[idx];
+            return _chunks[FlatIndex(idx.X, idx.Y, idx.Z)];
         }
 
         public void EnsureLoaded(Index3 idx)
         {
-            if (!_chunks.ContainsKey(idx))
-                _chunks[idx] = _loadDelegate(idx);
+            var flat = FlatIndex(idx.X, idx.Y, idx.Z);
+            if (_chunks[flat] == null)
+                _chunks[flat] = _loadDelegate(idx);
         }
 
         public void Release(Index3 idx)
         {
-            IChunk chunk;
-            if (_chunks.TryGetValue(idx, out chunk))
+            var flat = FlatIndex(idx.X, idx.Y, idx.Z);
+
+            var chunk = _chunks[flat];
+
+            if (chunk != null)
             {
                 _saveDelegate(idx, chunk);
-                _chunks.Remove(idx);
+                _chunks[flat] = null;
             }
         }
 
         public void Flush()
         {
-            foreach (var chunk in _chunks)
-              _saveDelegate(chunk.Key, chunk.Value);
+            for (int i = 0; i < _chunks.Length; i++)
+            {
+                var chunk = _chunks[i];
+                if(chunk != null)
+                    _saveDelegate(chunk.Index, chunk);
+            }
+        }
+
+        private int FlatIndex(int x, int y, int z)
+        {
+            return ((z & (ZMask)) << (XLimit + YLimit))
+                   | ((y & (YMask)) << XLimit)
+                   | ((x & (XMask)));
         }
     }
 }
