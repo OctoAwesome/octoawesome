74adda984929be2c655758e0ca5b0ecb7dd04e75
tomwendel <tom.wendel@rabbitcode.de>
2015-08-27 22:52:40 +0200

    Fehlende Datei eingefügt

---
+++ OctoAwesome/OctoAwesome/IChunkSerializer.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+
+namespace OctoAwesome
+{
+    public interface IChunkSerializer
+    {
+        void Serialize(Stream stream, IChunk chunk);
+
+        IChunk Deserialize(Stream stream, PlanetIndex3 position);
+    }
+}
