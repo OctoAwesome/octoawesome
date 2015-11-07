f91210e7e44f1d552bbfd20d79ce29146bb0cd73
Fabian Nawratil <f.nawratil@sbg.at>
2015-11-07 18:39:09 +0100

    Added Ability to make Crosshair Invisible (F11)

--- OctoAwesome/OctoAwesome.Client/Screens/GameScreen.cs
+++ OctoAwesome/OctoAwesome.Client/Screens/GameScreen.cs
@@ -15,6 +15,7 @@ namespace OctoAwesome.Client.Screens
         CompassControl compass;
         ToolbarControl toolbar;
         MinimapControl minimap;
+        CrosshairControl crosshair;
 
         public GameScreen(ScreenComponent manager) : base(manager)
         {
@@ -52,7 +53,7 @@ namespace OctoAwesome.Client.Screens
             minimap.Margin = Border.All(5);
             Controls.Add(minimap);
 
-            CrosshairControl crosshair = new CrosshairControl(manager);
+            crosshair = new CrosshairControl(manager);
             crosshair.HorizontalAlignment = HorizontalAlignment.Center;
             crosshair.VerticalAlignment = VerticalAlignment.Center;
             crosshair.Width = 8;
@@ -93,6 +94,7 @@ namespace OctoAwesome.Client.Screens
                 compass.Visible = !compass.Visible;
                 toolbar.Visible = !toolbar.Visible;
                 minimap.Visible = !minimap.Visible;
+                crosshair.Visible = !crosshair.Visible;
             }
 
             //Enable / Disable Debug
