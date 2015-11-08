a36488ea3be1cc0b3627cb0e2ac3a7bd7fd54f74
Fabian Nawratil <f.nawratil@sbg.at>
2015-11-08 19:58:35 +0100

    Added Crosshair

--- OctoAwesome/OctoAwesome.Client/Screens/GameScreen.cs
+++ OctoAwesome/OctoAwesome.Client/Screens/GameScreen.cs
@@ -18,6 +18,7 @@ namespace OctoAwesome.Client.Screens
         CompassControl compass;
         ToolbarControl toolbar;
         MinimapControl minimap;
+        CrosshairControl crosshair;
 
         public GameScreen(ScreenComponent manager) : base(manager)
         {
@@ -56,6 +57,13 @@ namespace OctoAwesome.Client.Screens
             minimap.Height = 128;
             minimap.Margin = Border.All(5);
             Controls.Add(minimap);
+
+            crosshair = new CrosshairControl(manager);
+            crosshair.HorizontalAlignment = HorizontalAlignment.Center;
+            crosshair.VerticalAlignment = VerticalAlignment.Center;
+            crosshair.Width = 8;
+            crosshair.Height = 8;
+            Controls.Add(crosshair);
 
             Title = "Game";
         }
@@ -276,6 +284,7 @@ namespace OctoAwesome.Client.Screens
                 compass.Visible = !compass.Visible;
                 toolbar.Visible = !toolbar.Visible;
                 minimap.Visible = !minimap.Visible;
+                crosshair.Visible = !crosshair.Visible;
             }
 
             //Enable / Disable Debug
