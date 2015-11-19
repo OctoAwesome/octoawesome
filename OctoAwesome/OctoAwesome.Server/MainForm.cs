61c8190c4948a0e2741c22b381b813ee5133971d
tomwendel <tom.wendel@rabbitcode.de>
2015-11-19 21:14:13 +0100

    [279] Server erzeugt

---
+++ OctoAwesome/OctoAwesome.Server/MainForm.cs
@@ -0,0 +1,27 @@
+﻿using OctoAwesome.Runtime;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OctoAwesome.Server
+{
+    public partial class MainForm : Form
+    {
+        private World world;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            world = new World();
+        }
+    }
+}
