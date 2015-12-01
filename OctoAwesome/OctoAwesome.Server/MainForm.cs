788dd701312406732d6f7eadbf061da6c539a869
Fabian Nawratil <f.nawratil@sbg.at>
2015-12-01 18:24:27 +0100

    Added Server Console

--- OctoAwesome/OctoAwesome.Server/MainForm.cs
+++ OctoAwesome/OctoAwesome.Server/MainForm.cs
@@ -19,10 +19,13 @@ namespace OctoAwesome.Server
         {
             InitializeComponent();
 
+            ServerConsole.Console = console_textBox;
+            ServerConsole.Log("Initializing...");
             Runtime.Server.Instance.OnRegister += Instance_OnRegister;
             Runtime.Server.Instance.OnDeregister += Instance_OnDeregister;
 
             Runtime.Server.Instance.Open();
+            ServerConsole.Log("---Server started---");
         }
 
         protected override void OnFormClosed(FormClosedEventArgs e)
@@ -36,12 +39,14 @@ namespace OctoAwesome.Server
         {
             //ListViewItem bestehend aus Playername & Planet
             listViewPlayers.Items.Remove(new ListViewItem(new String[] { client.Playername, "Default" }));
+            ServerConsole.Log("Player " + client.Playername + " left the game");
         }
 
         private void Instance_OnRegister(Client client)
         {
             //ListViewItem bestehend aus Playername & Planet
             listViewPlayers.Items.Add(new ListViewItem(new String[] { client.Playername, "Default" }));
+            ServerConsole.Log("Player " + client.Playername + " joined the game");
         }
 
         private void button_stopServer_Click(object sender, EventArgs e)
@@ -49,11 +54,13 @@ namespace OctoAwesome.Server
             if (((Button)sender).Text == "Stop")
             {
                 Runtime.Server.Instance.Close();
+                ServerConsole.Log("---Server started---");
                 ((Button)sender).Text = "Start";
             }
             else
             {
                 Runtime.Server.Instance.Open();
+                ServerConsole.Log("---Server stopped---");
                 ((Button)sender).Text = "Stop";
             }
         }
