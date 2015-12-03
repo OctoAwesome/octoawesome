c1fbb62facf305bb662815e693d3997798b997fa
tomwendel <tom.wendel@rabbitcode.de>
2015-12-03 21:32:48 +0100

    Client-Liste am Server veröffentlicht

--- OctoAwesome/OctoAwesome.Runtime/Server.cs
+++ OctoAwesome/OctoAwesome.Runtime/Server.cs
@@ -110,13 +110,13 @@ namespace OctoAwesome.Runtime
             }
         }
 
-        public IEnumerable<string> Clients
+        public IEnumerable<Client> Clients
         {
             get
             {
                 lock (clients)
                 {
-                    return clients.Select(c => c.Playername);
+                    return clients;
                 }
             }
         }
