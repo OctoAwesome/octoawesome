0a2c56297627a7915a42ce9a60c7f642cad9828e
Manuel Huber <ich@manuelhu.de>
2015-12-11 13:15:31 +0100

    ConsoleServer hinzugefügt

---
+++ OctoAwesome/OctoAwesome.ConsoleServer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OctoAwesome.Runtime;
+
+namespace OctoAwesome.ConsoleServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server.Instance.OnJoin += Instance_OnJoin;
+            Server.Instance.OnLeave += Instance_OnLeave;
+
+            Server.Instance.Open();
+
+            while (Console.ReadKey() != new ConsoleKeyInfo('c', ConsoleKey.C, false, false, false))
+            {
+
+            }
+
+            Server.Instance.Close();
+        }
+
+        private static void Instance_OnJoin(Client info)
+        {
+            Console.WriteLine("Client joined: " + info.Playername + " / Guid: " + info.ConnectionId.ToString());
+        }
+
+        private static void Instance_OnLeave(Client info)
+        {
+            Console.WriteLine("Client leaved: " + info.Playername + " / Guid: " + info.ConnectionId.ToString());
+        }
+    }
+}
