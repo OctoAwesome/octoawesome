d58fed24cb9fb4d591a7eab5214e345661a7f12f
tomwendel <tom.wendel@rabbitcode.de>
2016-01-22 01:27:07 +0100

    Community Assembly erstellt - Happy Developing!

---
+++ OctoAwesome/OctoAwesome.Community/UI/TestPlugin.cs
@@ -0,0 +1,33 @@
+﻿using MonoGameUi;
+using OctoAwesome.Client;
+using System.Collections.Generic;
+
+namespace OctoAwesome.Community.UI
+{
+    public class TestPlugin : IUiPlugin
+    {
+        public void MainMenuAdd(IScreenManager screenManager, ICollection<Control> controls)
+        {
+            var button = Button.TextButton(screenManager, "UIMods work!", "button");
+            button.HorizontalAlignment = HorizontalAlignment.Stretch;
+            button.Margin = new Border(0, 0, 0, 10);
+            button.LeftMouseClick += (s, e) => { screenManager.NavigateToScreen(new TestScreen(screenManager)); };
+            controls.Add(button);
+        }
+    }
+
+    public class TestScreen : Screen
+    {
+        public TestScreen(IScreenManager manager) : base(manager)
+        {
+            Padding = new Border(0, 0, 0, 0);
+
+            Button backButton = Button.TextButton(manager, "Back");
+            backButton.VerticalAlignment = VerticalAlignment.Top;
+            backButton.HorizontalAlignment = HorizontalAlignment.Left;
+            backButton.LeftMouseClick += (s, e) => { manager.NavigateBack(); };
+            backButton.Margin = new Border(10, 10, 10, 10);
+            Controls.Add(backButton);
+        }
+    }
+}
