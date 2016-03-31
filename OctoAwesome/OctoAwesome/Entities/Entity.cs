b0b25e4b321eb9d777b922241b84966f99f23a77
tomwendel <tom.wendel@rabbitcode.de>
2016-03-31 21:08:07 +0200

    GRML! Git hat mal wieder ne Datei verschluckt

---
+++ OctoAwesome/OctoAwesome/Entities/Entity.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using OctoAwesome.Entities;
+using System.Xml.Serialization;
+
+namespace OctoAwesome
+{
+    /// <summary>
+    /// Basisklasse für alle selbständigen Wesen
+    /// </summary>
+    public abstract class Entity: IPosition, IRotatable
+    {
+        private float angle = 0f;
+
+        /// <summary>
+        /// Die Position der Entität
+        /// </summary>
+        public Coordinate Position { get; set; }
+
+        /// <summary>
+        /// Blickwinkel in der horizontalen Achse
+        /// </summary>
+        public float Angle
+        {
+            get { return angle; }
+            set { angle = MathHelper.WrapAngle(value); }
+        }
+
+        /// <summary>
+        /// Blickwinkel in der vertikalen Achse
+        /// </summary>
+        public float Tilt { get; set; }
+    }
+}
