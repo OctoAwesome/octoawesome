bfe09f889d512b66efb8d62e83a276f593968a2c
Manuel Huber <ich@manuelhu.de>
2016-05-05 17:59:27 +0200

    MessageScreen hinzugefügt

---
+++ OctoAwesome/OctoAwesome.Client/Screens/MessageScreen.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGameUi;
+using OctoAwesome.Client.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OctoAwesome.Client.Screens
+{
+    internal sealed class MessageScreen : Screen
+    {
+        public MessageScreen(ScreenComponent manager, string title, string content, string button = "OK", Action<Control, MouseEventArgs> click = null) : base(manager)
+        {
+            IsOverlay = true;
+
+            Texture2D panelBackground = manager.Game.Content.LoadTexture2DFromFile("./Assets/OctoAwesome.Client/panel.png", manager.GraphicsDevice);
+            Background = NineTileBrush.FromSingleTexture(panelBackground, 30, 30);
+
+            HorizontalAlignment = HorizontalAlignment.Center;
+            VerticalAlignment = VerticalAlignment.Center;
+
+            StackPanel spanel = new StackPanel(manager);
+            Controls.Add(spanel);
+
+            Label headLine = new Label(manager);
+            headLine.Text = title;
+            headLine.Font = Skin.Current.HeadlineFont;
+            headLine.HorizontalAlignment = HorizontalAlignment.Stretch;
+            spanel.Controls.Add(headLine);
+
+            Label contentLabel = new Label(manager);
+            contentLabel.Text = content;
+            contentLabel.Font = Skin.Current.TextFont;
+            contentLabel.HorizontalAlignment = HorizontalAlignment.Stretch;
+            spanel.Controls.Add(contentLabel);
+
+            Button closeButton = Button.TextButton(manager, button);
+            closeButton.HorizontalAlignment = HorizontalAlignment.Stretch;
+            closeButton.LeftMouseClick += (s, e) => 
+            {
+                if (click != null)
+                    click(s, e);
+            };
+            spanel.Controls.Add(closeButton);
+        }
+    }
+}
