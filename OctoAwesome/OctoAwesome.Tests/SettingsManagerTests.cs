d9c3aef01bbd025f0acca19e9f32c17bf9e268f2
Johann Wagner <johann.wagner@st.ovgu.de>
2016-09-15 00:36:14 +0200

    SettingsManager kann nun Arrays schreiben und lesen.

--- OctoAwesome/OctoAwesome.Tests/SettingsManagerTests.cs
+++ OctoAwesome/OctoAwesome.Tests/SettingsManagerTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace OctoAwesome.Tests
@@ -9,8 +11,35 @@ namespace OctoAwesome.Tests
         [TestMethod]
         public void ReadWrite()
         {
+            SettingsManager.DEBUG = true;
+
+            string[] testArray = new string[] {"foo", "bar"};
+            SettingsManager.Set("foo", testArray);
+            
+            string[] newArray = SettingsManager.GetArray<string>("foo");
+
+            Assert.IsTrue(testArray.SequenceEqual(newArray));
+
+
+            int[] testArrayInt = new int[] { 3,5,333,456,3457};
+            SettingsManager.Set("fooInt", testArrayInt);
+
+            int[] newArrayInt = SettingsManager.GetArray<int>("fooInt");
+
+            Assert.IsTrue(testArray.SequenceEqual(newArray));
+
+
+            bool[] testArrayBool = new bool[] { true, false};
+            SettingsManager.Set("fooBool", testArrayBool);
+
+            bool[] newArrayBool = SettingsManager.GetArray<bool>("fooBool");
+
+            Assert.IsTrue(testArray.SequenceEqual(newArray));
+
+
             String inputString = "randomStringWith§$%&/()=Charakters";
             SettingsManager.Set("inputString", inputString);
+
 
             Assert.AreEqual(inputString, SettingsManager.Get<string>("inputString"));
 
@@ -25,6 +54,7 @@ namespace OctoAwesome.Tests
             SettingsManager.Set("inputBool", inputBool);
 
             Assert.AreEqual(inputBool, SettingsManager.Get<bool>("inputBool"));
+
 
 
         }
