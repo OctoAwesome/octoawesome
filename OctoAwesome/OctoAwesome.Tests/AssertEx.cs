99e763f61a4e36cb4b6eabe2a525e82eb903b66c
Paul Reichelt <paul.reichelt@outlook.com>
2016-10-16 18:06:09 +0200

    Fixed Migration Error with double compare precission

--- OctoAwesome/OctoAwesome.Tests/AssertEx.cs
+++ OctoAwesome/OctoAwesome.Tests/AssertEx.cs
@@ -7,9 +7,9 @@ namespace OctoAwesome.Tests
     {
         public static void Equal(Vector3 p1, Vector3 p2)
         {
-            Assert.Equal(p1.X, p2.X);
-            Assert.Equal(p1.Y, p2.Y);
-            Assert.Equal(p1.Z, p2.Z);
+            Assert.Equal(p1.X, p2.X, 4);
+            Assert.Equal(p1.Y, p2.Y, 4);
+            Assert.Equal(p1.Z, p2.Z, 4);
         }
     }
 }
