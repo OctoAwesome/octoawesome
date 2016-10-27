959c917a3cc9dccdfcac73d33e75e99835c74ba9
tomwendel <tom.wendel@rabbitcode.de>
2016-10-27 22:27:26 +0200

    SimulationComponent von grünem Schaf und Entity Filter

--- OctoAwesome/OctoAwesome.Basics/EntityComponents/MoveableComponent.cs
+++ OctoAwesome/OctoAwesome.Basics/EntityComponents/MoveableComponent.cs
@@ -9,6 +9,8 @@ namespace OctoAwesome.Basics.EntityComponents
 {
     public sealed class MoveableComponent : EntityComponent
     {
+        public Vector3 ExternalForces { get; set; }
+
         public Vector3 Velocity { get; set; }
 
         public Vector3 Move { get; set; }
--- OctoAwesome/OctoAwesome/SimulationComponent.cs
+++ OctoAwesome/OctoAwesome/SimulationComponent.cs
@@ -9,10 +9,72 @@ namespace OctoAwesome
 {
     public abstract class SimulationComponent : Component
     {
-        public abstract void AddEntity(Entity entity);
+        protected List<Entity> entities = new List<Entity>();
 
-        public abstract void RemoveEntity(Entity entity);
+        private List<Type[]> componentFilter = new List<Type[]>();
+
+        public SimulationComponent()
+        {
+            // TODO: Refelct Attributes
+            foreach(EntityFilterAttribute attribute in GetType().GetCustomAttributes(typeof(EntityFilterAttribute), false))
+            {
+                foreach (var entityComponentType in attribute.EntityComponentTypes)
+                {
+                    if (!typeof(EntityComponent).IsAssignableFrom(entityComponentType))
+                        throw new NotSupportedException();
+
+                    componentFilter.Add(attribute.EntityComponentTypes);
+                }
+            }
+        }
+
+        public void Add(Entity entity)
+        {
+            if (Match(entity) && AddEntity(entity))
+            {
+                entities.Add(entity);
+            }
+        }
+
+        protected virtual bool Match(Entity entity)
+        {
+            return componentFilter.Any(
+                x => x.All(
+                    t => entity.Components.Any(
+                        c => t.IsAssignableFrom(c.GetType()))));
+        }
+
+        protected abstract bool AddEntity(Entity entity);
+
+        public void Remove(Entity entity)
+        {
+            if (entities.Contains(entity))
+            {
+                RemoveEntity(entity);
+                entities.Remove(entity);
+            }
+        }
+
+        protected abstract void RemoveEntity(Entity entity);
 
         public abstract void Update(GameTime gameTime);
+    }
+
+    public abstract class SimulationComponent<C1> : SimulationComponent where C1 : EntityComponent
+    {
+        protected override bool Match(Entity entity)
+        {
+            return entity.Components.ContainsComponent<C1>();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            foreach (var entity in entities)
+            {
+                UpdateEntity(entity, entity.Components.GetComponent<C1>());
+            }
+        }
+
+        protected abstract void UpdateEntity(Entity e, C1 component1);
     }
 }
