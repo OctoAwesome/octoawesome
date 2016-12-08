6ab57dfbdce25e776bc79887e8882d92653422bd
Lassi <csharplassi@posteo.de>
2016-12-08 21:20:58 +0100

    Block Apply reimplemented

--- OctoAwesome/OctoAwesome.Basics/SimulationComponents/BlockInteractionComponent.cs
+++ OctoAwesome/OctoAwesome.Basics/SimulationComponents/BlockInteractionComponent.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using engenious;
+using OctoAwesome.Basics.EntityComponents;
 
 namespace OctoAwesome.Basics.SimulationComponents
 {
@@ -30,6 +31,8 @@ namespace OctoAwesome.Basics.SimulationComponents
 
         protected override void UpdateEntity(GameTime gameTime, Entity entity, ControllableComponent controller, InventoryComponent inventory)
         {
+            var toolbar = entity.Components.GetComponent<ToolBarComponent>();
+
             if (controller.InteractBlock.HasValue)
             {
                 ushort lastBlock = entity.Cache.GetBlock(controller.InteractBlock.Value);
@@ -51,7 +54,7 @@ namespace OctoAwesome.Basics.SimulationComponents
                         };
                         inventory.Inventory.Add(slot);
 
-                        var toolbar = entity.Components.GetComponent<ToolBarComponent>();
+                        
                         if (toolbar != null)
                         {
                             for (int i = 0; i < toolbar.Tools.Length; i++)
@@ -67,6 +70,80 @@ namespace OctoAwesome.Basics.SimulationComponents
                     slot.Amount += 125;
                 }
                 controller.InteractBlock = null;
+            }
+
+            if (toolbar != null && controller.ApplyBlock.HasValue)
+            {
+                if (toolbar.ActiveTool != null)
+                {
+                    Index3 add = new Index3();
+                    switch (controller.ApplySide)
+                    {
+                        case OrientationFlags.SideWest: add = new Index3(-1, 0, 0); break;
+                        case OrientationFlags.SideEast: add = new Index3(1, 0, 0); break;
+                        case OrientationFlags.SideSouth: add = new Index3(0, -1, 0); break;
+                        case OrientationFlags.SideNorth: add = new Index3(0, 1, 0); break;
+                        case OrientationFlags.SideBottom: add = new Index3(0, 0, -1); break;
+                        case OrientationFlags.SideTop: add = new Index3(0, 0, 1); break;
+                    }
+
+                    if (toolbar.ActiveTool.Definition is IBlockDefinition)
+                    {
+                        IBlockDefinition definition = toolbar.ActiveTool.Definition as IBlockDefinition;
+
+                        Index3 idx = controller.ApplyBlock.Value + add;
+                        var boxes = definition.GetCollisionBoxes(entity.Cache, idx.X, idx.Y, idx.Z);
+
+                        bool intersects = false;
+                        var positioncomponent = entity.Components.GetComponent<PositionComponent>();
+                        var bodycomponent = entity.Components.GetComponent<BodyComponent>();
+
+                        if (positioncomponent != null && bodycomponent != null)
+                        {
+                            float gap = 0.01f;
+                            var playerBox = new BoundingBox(
+                                new Vector3(
+                                    positioncomponent.Position.GlobalBlockIndex.X + positioncomponent.Position.BlockPosition.X - bodycomponent.Radius + gap,
+                                    positioncomponent.Position.GlobalBlockIndex.Y + positioncomponent.Position.BlockPosition.Y - bodycomponent.Radius + gap,
+                                    positioncomponent.Position.GlobalBlockIndex.Z + positioncomponent.Position.BlockPosition.Z + gap),
+                                new Vector3(
+                                    positioncomponent.Position.GlobalBlockIndex.X + positioncomponent.Position.BlockPosition.X + bodycomponent.Radius - gap,
+                                    positioncomponent.Position.GlobalBlockIndex.Y + positioncomponent.Position.BlockPosition.Y + bodycomponent.Radius - gap,
+                                    positioncomponent.Position.GlobalBlockIndex.Z + positioncomponent.Position.BlockPosition.Z + bodycomponent.Height - gap)
+                                );
+
+                            // Nicht in sich selbst reinbauen
+                            
+                            foreach (var box in boxes)
+                            {
+                                var newBox = new BoundingBox(idx + box.Min, idx + box.Max);
+                                if (newBox.Min.X < playerBox.Max.X && newBox.Max.X > playerBox.Min.X &&
+                                    newBox.Min.Y < playerBox.Max.Y && newBox.Max.X > playerBox.Min.Y &&
+                                    newBox.Min.Z < playerBox.Max.Z && newBox.Max.X > playerBox.Min.Z)
+                                    intersects = true;
+                            }
+                        }
+                       
+
+                        if (!intersects)
+                        {
+                            entity.Cache.SetBlock(idx, simulation.ResourceManager.DefinitionManager.GetBlockDefinitionIndex(definition));
+
+                            toolbar.ActiveTool.Amount -= 125;
+                            if (toolbar.ActiveTool.Amount <= 0)
+                            {
+                                inventory.Inventory.Remove(toolbar.ActiveTool);
+                                for (int i = 0; i < toolbar.Tools.Length; i++)
+                                {
+                                    if (toolbar.Tools[i] == toolbar.ActiveTool)
+                                        toolbar.Tools[i] = null;
+                                }
+                                toolbar.ActiveTool = null;
+                            }
+                        }
+                    }
+                    controller.ApplyBlock = null;
+                }
             }
         }
     }
