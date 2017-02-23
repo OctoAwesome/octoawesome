c0af78c2e4ab7fc519ca8917bbe1a8ab5872cc46
tomwendel <tom.wendel@rabbitcode.de>
2017-02-23 22:42:24 +0100

    Wauzi Egg als Item hinzugefügt

---
+++ OctoAwesome/OctoAwesome.Basics/Definitions/Items/WauziEggDefinition.cs
@@ -0,0 +1,37 @@
+﻿namespace OctoAwesome.Basics.Definitions.Items
+{
+    public sealed class WauziEggDefinition : IItemDefinition
+    {
+        public string Icon
+        {
+            get
+            {
+                return "wauziegg";
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return "Wauzi Egg";
+            }
+        }
+
+        public int StackLimit
+        {
+            get
+            {
+                return 1000;
+            }
+        }
+
+        public float VolumePerUnit
+        {
+            get
+            {
+                return 1;
+            }
+        }
+    }
+}
