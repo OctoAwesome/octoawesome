a9a0efa2f39c9a25c7e48048c7f2d4977d34c366
Marcus Aurelius <Marcus.Aurelius@gallimathias.de>
2018-01-04 20:45:10 +0100

    #224 Remove reduntant code
    Restructure inheritance.

--- OctoAwesome/OctoAwesome.Basics/Biomes/HighMountainBiome.cs
+++ OctoAwesome/OctoAwesome.Basics/Biomes/HighMountainBiome.cs
@@ -6,50 +6,12 @@ using System.Text;
 
 namespace OctoAwesome.Basics.Biomes
 {
-    class HighMountainBiome : IBiome
+    class HighMountainBiome : BiomeBase
     {
-
-        public IPlanet Planet { get; private set; }
-
-        public List<IBiome> SubBiomes { get; private set; }
-
-        public INoise BiomeNoiseGenerator { get; private set; }
-
-        public float MinValue { get; private set; }
-
-        public float MaxValue { get; private set; }
-
-        public float ValueRangeOffset { get; private set; }
-
-        public float ValueRange { get; private set; }
-
         public HighMountainBiome(IPlanet planet, float minValue, float maxValue, float valueRangeOffset, float valueRange)
+            : base(planet, minValue, maxValue, valueRangeOffset, valueRange)
         {
-            this.BiomeNoiseGenerator = new SimplexNoiseGenerator(planet.Seed + 2) { FrequencyX = 1f / 256, FrequencyY = 1f / 256, FrequencyZ = 1f / 256, Persistance = 0.5f, Octaves = 6, Factor = 1f };
-            this.Planet = planet;
-            this.MinValue = minValue;
-            this.MaxValue = maxValue;
-            this.ValueRangeOffset = valueRangeOffset;
-            this.ValueRange = valueRange;
-        }
-
-        public float[,] GetHeightmap(Index2 chunkIndex)
-        {
-
-            float[,] values = new float[Chunk.CHUNKSIZE_X, Chunk.CHUNKSIZE_Y];
-
-            chunkIndex = new Index2(chunkIndex.X * Chunk.CHUNKSIZE_X, chunkIndex.Y * Chunk.CHUNKSIZE_Y);
-
-            float[,] heights = BiomeNoiseGenerator.GetTileableNoiseMap2D(chunkIndex.X, chunkIndex.Y, Chunk.CHUNKSIZE_X, Chunk.CHUNKSIZE_Y, Planet.Size.X * Chunk.CHUNKSIZE_X, Planet.Size.Y * Chunk.CHUNKSIZE_Y);
-
-            for (int x = 0; x < Chunk.CHUNKSIZE_X; x++)
-            {
-                for (int y = 0; y < Chunk.CHUNKSIZE_Y; y++)
-                {
-                    values[x, y] = (heights[x, y] / 2 + 0.5f) * ValueRange + ValueRangeOffset;
-                }
-            }
-            return values;
+            BiomeNoiseGenerator = new SimplexNoiseGenerator(planet.Seed + 2) { FrequencyX = 1f / 256, FrequencyY = 1f / 256, FrequencyZ = 1f / 256, Persistance = 0.5f, Octaves = 6, Factor = 1f };
         }
     }
 }
---
+++ OctoAwesome/OctoAwesome.Basics/Biomes/LargeBiomeBase.cs
@@ -0,0 +1,94 @@
+﻿using OctoAwesome.Noise;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OctoAwesome.Basics.Biomes
+{
+    public abstract class LargeBiomeBase : BiomeBase
+    {
+
+
+        public LargeBiomeBase(IPlanet planet, float valueRangeOffset, float valueRange)
+            : base(planet, 0, 0, valueRangeOffset, valueRange)
+        {
+        }
+
+        protected void SortSubBiomes()
+        {
+            SubBiomes = SubBiomes.OrderBy(a => a.MinValue).ToList();
+
+            if (SubBiomes.Count > 0 && (SubBiomes.First().MinValue > 0f || SubBiomes.Last().MaxValue < 1f))
+            {
+                throw new InvalidOperationException("MinValue oder MaxValue der Biome nicht in gültigem Bereich");
+            }
+        }
+
+        protected IBiome ChooseBiome(float value, out IBiome secondBiome)
+        {
+            secondBiome = null;
+            bool betweenPossible = false;
+            for (int i = 0; i < SubBiomes.Count; i++)
+            {
+                if (betweenPossible && value < SubBiomes[i].MinValue)
+                {
+                    secondBiome = SubBiomes[i];
+                    return SubBiomes[i - 1];
+                }
+                else if (SubBiomes[i].MaxValue >= value && SubBiomes[i].MinValue <= value)
+                    return SubBiomes[i];
+                betweenPossible = (value > SubBiomes[i].MaxValue);
+            }
+            return null;
+        }
+
+        protected int ChooseBiome(float value, out int secondBiome)
+        {
+            secondBiome = -1;
+            bool betweenPossible = false;
+            for (int i = 0; i < SubBiomes.Count; i++)
+            {
+                if (betweenPossible && value < SubBiomes[i].MinValue)
+                {
+                    secondBiome = i;
+                    return i - 1;
+                }
+                else if (SubBiomes[i].MaxValue >= value && SubBiomes[i].MinValue <= value)
+                    return i;
+                betweenPossible = (value > SubBiomes[i].MaxValue);
+            }
+            return -1;
+        }
+
+        protected float CalculateInterpolationValue(float region, out IBiome biome1, out IBiome biome2)
+        {
+            biome1 = ChooseBiome(region, out biome2);
+
+            return CalculateInterpolationValue(region, biome1, biome2);
+        }
+
+        protected float CalculateInterpolationValue(float region, IBiome biome1, IBiome biome2)
+        {
+            if (biome2 != null)
+            {
+                float diff = biome2.MinValue - biome1.MaxValue;
+                region -= biome1.MaxValue;
+                region /= diff;
+                return CurveFunction(region);
+            }
+            else if (biome1 != null)
+            {
+                return 0f;
+            }
+            return 0f;
+        }
+
+        protected virtual float CurveFunction(float inputValue)
+        {
+            return inputValue;
+        }
+
+        public override float[,] GetHeightmap(Index2 chunkIndex) => base.GetHeightmap(chunkIndex);
+    }
+}
