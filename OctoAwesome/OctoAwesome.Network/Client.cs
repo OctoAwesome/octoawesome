0980b124759dde4f754405950156b2ef4344161c
Marcus Aurelius <Marcus.Aurelius@gallimathias.de>
2018-01-18 21:56:28 +0100

    #230 Start implement TcpServer

---
+++ OctoAwesome/OctoAwesome.Network/Client.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System.Net.Sockets;
+
+namespace OctoAwesome.Network
+{
+    public class Client
+    {
+        //public NetworkStream TcpStream => tcpClient.GetStream();
+
+        public Socket Socket { get; private set; }
+
+        private SocketAsyncEventArgs socketArgs;
+
+        public Client(Socket socket)
+        {
+            Socket = socket;
+            socketArgs = new SocketAsyncEventArgs();
+            socketArgs.Completed += SocketArgsCompleted;
+        }
+        
+        public void ReceiveAsync()
+        {
+            Socket.ReceiveAsync(socketArgs);
+        }
+
+        public void SendAsync(byte[] data)
+        {
+            socketArgs.SetBuffer(data, 0, data.Length);
+            Socket.SendAsync(socketArgs);
+        }
+
+        private void SocketArgsCompleted(object sender, SocketAsyncEventArgs e)
+        {
+        }
+
+    }
+}
---
+++ OctoAwesome/OctoAwesome.Network/Server.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OctoAwesome.Network
+{
+    class Server
+    {
+        public List<Client> Clients { get; set; }
+
+        private TcpListener tcpListener;
+
+
+        public Server()
+        {
+            tcpListener = new TcpListener(IPAddress.Any, 44444); //TODO: variable port?
+        }
+
+        public void Start()
+        {
+            tcpListener.Start();
+            var socketArgs = new SocketAsyncEventArgs();
+            socketArgs.Completed += OnClientConnect;
+            tcpListener.Server.AcceptAsync(socketArgs);
+
+            //tcpListener.BeginAcceptTcpClient(OnClientConnect, null);
+        }
+
+        private void OnClientConnect(object sender, SocketAsyncEventArgs e)
+        {
+            var client = e.ConnectSocket;
+            tcpListener.Server.AcceptAsync(e);
+            
+            //TODO Handshake aka Registration and authentication ????
+            Clients.Add(new Client(e));
+
+        }
+
+        public void Stop()
+        {
+            tcpListener.Stop();
+        }
+    }
+}
