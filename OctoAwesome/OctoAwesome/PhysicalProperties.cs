d71b25589e6e404036a4522762b98847dac17c81
Maximilian Krüger <Marcus.Aurelius@gallimathias.de>
2020-08-26 21:36:35 +0200

    Add comments to physical props
    * Add comments to the physical props so we know in future what we find out today 😀
    
    Co-authored-by: susch19 <suschi@ist-einmalig.de>

--- OctoAwesome/OctoAwesome/PhysicalProperties.cs
+++ OctoAwesome/OctoAwesome/PhysicalProperties.cs
@@ -6,22 +6,22 @@
     public class PhysicalProperties
     {
         /// <summary>
-        /// Härte
+        /// Härte, welche Materialien können abgebaut werden
         /// </summary>
         public float Hardness { get; set; }
 
         /// <summary>
-        /// Dichte in kg/dm^3
+        /// Dichte in kg/dm^3, Wie viel benötigt (Volumen berechnung) für Crafting bzw. hit result etc....
         /// </summary>
         public float Density { get; set; }
 
         /// <summary>
-        /// Granularität
+        /// Granularität, Effiktivität von "Materialien" Schaufel für hohe Werte, Pickaxe für niedrige
         /// </summary>
         public float Granularity { get; set; }
 
         /// <summary>
-        /// Bruchzähigkeit
+        /// Bruchzähigkeit, Wie schnell geht etwas zu bruch? Haltbarkeit.
         /// </summary>
         public float FractureToughness { get; set; }
     }
