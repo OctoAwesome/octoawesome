8ed728139f2fbef13163a67683199a4298f4a41d
Maximilian Krüger <Marcus.Aurelius@gallimathias.de>
2021-05-19 22:10:40 +0200

    Try to develope two solutions for dependency resolving
    * Try to resolve the dependency graph order with two diffrent solutions
    
    Co-authored-by: susch19 <suschi@ist-einmalig.de>

--- OctoAwesome/OctoAwesome.PoC.Tests/UnitTest1.cs
+++ OctoAwesome/OctoAwesome.PoC.Tests/UnitTest1.cs
@@ -1,7 +1,7 @@
 using NUnit.Framework;
 
 using OpenTK.Windowing.Desktop;
-
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
@@ -79,8 +79,19 @@ namespace OctoAwesome.PoC.Tests
                 new DependencyItem(typeof(long), "Long", new List<string> { }, new List<string> { "ULong" }),
                 new DependencyItem(typeof(uint), "UInt", new List<string> { "ULong" }, new List<string> { }),
                 new DependencyItem(typeof(ulong), "ULong", new List<string> { "String" }, new List<string> { }),
+                new DependencyItem(typeof(ulong), "UShort", new List<string> { "String", "Long", "Bool", "Int" }, new List<string> { }),
+                new DependencyItem(typeof(ulong), "Short", new List<string> { "Test" }, new List<string> {"Test2" }),
             };
-            var dic = dependencies.ToDictionary(x => x.Name, x => new RefCount(x, new(), new()));
+
+            //String => Bool => Long => ULong => Uint => Int
+            //Bool => String => Long => ULong => Uint => Int
+            var dic = dependencies.ToDictionary(x => x.Name, x => new RefCount(x, new(), new(), 0));
+
+
+            //Wenn kein Before: Muss nur nach besagten After geladen werden. Positionierung sonst irrelevant
+            //Wenn kein After: Muss nur vor besagten Before geladen werden. Positionierung sonst irrelevant
+            //Wenn beides nicht: Überall
+            //Wenn beides vorhanden: Schwierig, irgendwo dazwischen
 
             foreach (var item in dependencies)
             {
@@ -93,6 +104,7 @@ namespace OctoAwesome.PoC.Tests
                     if (!dic.TryGetValue(after, out var afterCount))
                         continue;
                     afterCount.Before.Add(refCount);
+                    refCount.After.Add(afterCount);
                 }
 
                 foreach (var before in item.BeforeDependencyItems)
@@ -100,19 +112,220 @@ namespace OctoAwesome.PoC.Tests
                     if (!dic.TryGetValue(before, out var beforeCount))
                         continue;
                     beforeCount.After.Add(refCount);
+                    refCount.Before.Add(beforeCount);
                 }
             }
             //Insert Dependencies to Path Logic here
 
+
+            // BOOL => LONG => STRING => Ulong => INT/UINT
+            // bool => Long => string => Ulong => int/uint
+
+            //SeperateWay(dic.Select(v => v.Value));
+            CalcDummy(dic.Select(v => v.Value));
+            List<RefCount> tree = new();
+            HashSet<string> LoadTreeNode(RefCount refCount, HashSet<string> alreadyDoneItems, int minIndex = 0, int maxIndex = int.MaxValue)
+            {
+                if (alreadyDoneItems.Contains(refCount.Item.Name))
+                    return alreadyDoneItems;
+
+                alreadyDoneItems.Add(refCount.Item.Name);
+
+                bool anyIndex = false;
+
+                int indexLeft = minIndex;
+
+                foreach (var ba in refCount.After)
+                {
+                    int existIndex = tree.IndexOf(ba, minIndex);
+                    if (existIndex > -1 && existIndex > indexLeft - 1)
+                    {
+                        anyIndex = true;
+                        indexLeft = existIndex + 1;
+                    }
+                }
+                int indexRight = int.MaxValue;
+
+                foreach (var ba in refCount.Before)
+                {
+                    int existIndex = tree.IndexOf(ba, minIndex);
+                    if (existIndex > -1 && existIndex < maxIndex && existIndex < indexRight)
+                    {
+                        anyIndex = true;
+                        indexRight = existIndex;
+                    }
+                }
+
+                if (indexLeft > indexRight)
+                    throw new System.Exception("No possible slot found");
+
+                var insertIndex = indexRight == int.MaxValue ? indexLeft : indexRight;
+                if (!anyIndex)
+                    tree.Add(refCount);
+                else
+                    tree.Insert(insertIndex, refCount);
+
+                foreach (var ba in refCount.After)
+                    LoadTreeNode(ba, alreadyDoneItems);
+                foreach (var ba in refCount.Before)
+                    LoadTreeNode(ba, alreadyDoneItems);
+
+                return alreadyDoneItems;
+            }
+
+            var orderedDic = dic
+                .OrderByDescending(x => x.Value.After.Count + x.Value.Before.Count)
+                ;
+
+            HashSet<string> loadedItemNames = new();
+            foreach (var ba in orderedDic)
+            {
+                LoadTreeNode(ba.Value, loadedItemNames);
+            }
+
+
+
+
             Assert.Pass();
         }
 
-        record RefCount(DependencyItem Item, List<RefCount> Before, List<RefCount> After)
+        private void SeperateWay(IEnumerable<RefCount> refCounts)
         {
+            /*                  0(3)    -> 5(7)
+             *                  1(4)
+             *                  2       -> 1(8)
+             *         2(1) <-  3
+             *         2    <-  4(5)
+             *         3(2) <-  5(6)    -> 1 (9),4(10)
+
+            2,3,0,5,1,4
+            Bool,Long,String,Ulong,Int,Uint
+
+            */
+            var index = 1;
+
+            foreach (var item in refCounts)
+            {
+                foreach (var beforeRef in item.After)
+                {
+                    if (beforeRef.Position == 0)
+                    {
+                        beforeRef.Position = index;
+                        index++;
+                    }
+                }
+            }
+
+            foreach (var item in refCounts)
+            {
+                if (item.Position == 0)
+                {
+                    item.Position = index;
+                    index++;
+                }
+            }
+
+            var indexBeforeAfter = index;
+
+            foreach (var item in refCounts.OrderBy(x => x.Position))
+            {
+                foreach (var beforeRef in item.Before)
+                {
+                    if (beforeRef.Position > indexBeforeAfter)
+                        continue;
+
+                    beforeRef.Position = index;
+                    index++;
+                }
+            }
+
+            var result = refCounts.OrderBy(r => r.Position).Select(r => r.Item.Name);
+            var resultstring = string.Join("=>", result);
+        }
+
+
+        private void CalcDummy(IEnumerable<RefCount> refCounts)
+        {
+            /*
+             *                      0       -> 5,6
+             *                      1       -> 6
+             *                      2       -> 1,6   
+             *         2    <-      3       -> 6
+             *         2    <-      4   
+             *         3    <-      5       -> 1,4 
+             *                      6
+             *                  
+             *        0 = 0
+             *        1 = 2
+             *        2 = 0
+             *        3 = 1
+             *        4 = 2
+             *        5 = 2
+             *        6 = 3
+             *        
+             *        0,2,3,1,4,5,6
+            */
+
+            var index = 0;
+
+            foreach (var item in refCounts)
+            {
+                CheckAndMove(item, null);
+            }
+
+            var result = refCounts.OrderBy(r => r.Position).Select(r => r.Item.Name);
+            var resultstring = string.Join("=>", result);
+        }
+
+        private static void CheckAndMove(RefCount item, RefCount parent)
+        {
+            var maxLef = item.After.Count > 0 ? item.After.Max(r => r.Position) : 0;
+
+            if (item.Position <= maxLef)
+            {
+                item.Position = maxLef + 1;               
+            }
+
+            foreach (var rightItem in item.Before)
+            {
+                if(parent == rightItem)
+                {
+                    throw new ArgumentException();
+                }
+
+                if (rightItem.Position <= item.Position)
+                {
+                    rightItem.Position = item.Position + 1;
+                    CheckAndMove(rightItem, item);
+                }
+            }
+        }
+
+        class RefCount
+        {
+            public RefCount(DependencyItem Item, List<RefCount> Before, List<RefCount> After, int Position)
+            {
+                this.Item = Item;
+                this.Before = Before;
+                this.After = After;
+                this.Position = Position;
+            }
+
+            public DependencyItem Item { get; }
+            /// <summary>
+            /// This loads before these items
+            /// </summary>
+            public List<RefCount> Before { get; }
+            /// <summary>
+            /// This loads after these items
+            /// </summary>
+            public List<RefCount> After { get; }
+            public int Position { get; set; }
+
             public override string ToString()
 
-                => $"{Item.Name}  Loads {Before.Count} Before:{string.Join(", ", Before.Select(x=>x.Item.Name))}  Loads {After.Count} After:{string.Join(", ", After.Select(x => x.Item.Name))}";
-            
+                => $"{Item.Name} loads Before: {string.Join(", ", Before.Select(x => x.Item.Name))} and loads After: {string.Join(", ", After.Select(x => x.Item.Name))} (BeforeCount: {Before.Count}, AfterCount: {After.Count})";
+
         }
     }
 }
