95c4c4b32f180df0c713a499362b9ed1c41cc0f0
Maximilian Krüger <Marcus.Aurelius@gallimathias.de>
2021-11-24 22:09:11 +0100

    Discussion and Evaluation
    * Discuss and Evaluate the UIComponent design to be sure what we have to do in the next stream
    
    Co-authored-by: susch19 <suschi@ist-einmalig.de>

---
+++ OctoAwesome/OctoAwesome.Client.UI/Components/UIComponent.cs
@@ -0,0 +1,21 @@
+﻿using engenious;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OctoAwesome.UI.Components
+{
+    /// <summary>
+    /// Component to provide a ui for the screen
+    /// </summary>
+    public abstract class UIComponent : Component
+    {
+        public bool Visible { get; set; }
+        /// <summary>
+        /// Displays the ui
+        /// </summary>
+        public abstract void Draw(GameTime gameTime);
+    }
+}
