6a2b6e38b8cd2ece54f170cb69571f6068c00a49
susch19 <suschi@ist-einmalig.de>
2022-03-20 16:13:53 +0100

    Implemented non threadsafe list that can be enumerated
    * for the cases, where concurrency might not be needed or even wanted
    * readded the wauzi egg, but this time it spawn a wauzi on use, so it's no longer a placeholder

---
+++ OctoAwesome/OctoAwesome.Basics/Definitions/Items/WauziItem.cs
@@ -0,0 +1,58 @@
+﻿using engenious;
+
+using OctoAwesome.Basics.Entities;
+using OctoAwesome.Basics.FunctionBlocks;
+using OctoAwesome.Chunking;
+using OctoAwesome.Definitions;
+using OctoAwesome.Definitions.Items;
+using OctoAwesome.EntityComponents;
+using OctoAwesome.Information;
+using OctoAwesome.Location;
+using OctoAwesome.Notifications;
+using OctoAwesome.Rx;
+using OctoAwesome.Services;
+
+using System;
+
+namespace OctoAwesome.Basics.Definitions.Items
+{
+    public class WauziItem : Item, IDisposable
+    {
+        public override int VolumePerUnit => base.VolumePerUnit;
+
+        public override int StackLimit => base.StackLimit;
+
+
+        private readonly Relay<Notification> simulationRelay;
+        private readonly IDisposable simulationSource;
+
+        public WauziItem(WauziItemDefinition definition, IMaterialDefinition materialDefinition)
+            : base(definition, materialDefinition)
+        {
+            var updateHub = TypeContainer.Get<IUpdateHub>();
+            simulationRelay = new Relay<Notification>();
+
+            simulationSource = updateHub.AddSource(simulationRelay, DefaultChannels.Simulation);
+        }
+
+        public override int Apply(IMaterialDefinition material, IBlockInteraction hitInfo, decimal volumeRemaining)
+        {
+            BlockInteractionService.CalculatePositionAndRotation(hitInfo, out var index3, out var _);
+
+            WauziEntity wauzi = new WauziEntity();
+
+            PositionComponent position = new PositionComponent() { Position = new Coordinate(0, index3, new Vector3(0, 0, 0)) };
+            wauzi.Components.AddComponent(position);
+
+            simulationRelay.OnNext(new EntityNotification(EntityNotification.ActionType.Add, wauzi));
+
+            return 0;
+        }
+
+        public void Dispose()
+        {
+            simulationSource.Dispose();
+            simulationRelay?.Dispose();
+        }
+    }
+}
---
+++ OctoAwesome/OctoAwesome.Basics/Definitions/Items/WauziItemDefinition.cs
@@ -0,0 +1,28 @@
+﻿using OctoAwesome.Basics.Definitions.Items.Food;
+using OctoAwesome.Definitions;
+using OctoAwesome.Definitions.Items;
+
+namespace OctoAwesome.Basics.Definitions.Items
+{
+    public class WauziItemDefinition : IItemDefinition
+    {
+        public string Name { get; }
+        public string Icon { get; }
+
+        public WauziItemDefinition()
+        {
+            Name = "Wauziegg";
+            Icon = "wauziegg";
+        }
+
+        public bool CanMineMaterial(IMaterialDefinition material)
+            => false;
+
+        public Item Create(IMaterialDefinition material)
+        {
+            if (material is not IFoodMaterialDefinition md)
+                return null;
+            return new WauziItem(this, material);
+        }
+    }
+}
