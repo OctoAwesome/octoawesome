dc8632d84f18758f369bb2762272a4eb9f6f74b2
susch19 <suschi@ist-einmalig.de>
2024-10-16 21:51:25 +0200

    Implemented source generator for const strings
    * so that the definitions are converted to const strings for typo safety
    * use the newly generated const strings
    * create a seperate const strings file for things like methods and the special hand item

---
+++ OctoAwesome/OctoAwesome.Basics/ConstStrings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public partial class ConstStrings
+{
+    public const string CreateItem = nameof(CreateItem);
+    public const string PlantTree = nameof(PlantTree);
+    public const string GetTextureIndex = nameof(GetTextureIndex);
+    public const string GetTextureRotation = nameof(GetTextureRotation);
+    public const string CreateNode = nameof(CreateNode);
+    public const string CanMineMaterial = nameof(CanMineMaterial);
+
+    public const string BaseHandCoreItem = "base_hand@core.item";
+}
